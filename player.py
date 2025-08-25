8983e7780e535ad80b93ce95133b4f4718d9b1cf
Erin Czarnecki <87948718+erinczarnecki@users.noreply.github.com>
2025-08-24 20:33:05 -0500

    define shared class, player class, and trader (npc) class

---
+++ player.py
@@ -0,0 +1,12 @@
+# This class inherits the logic from the BaseTrader class and is intended for the user-led game flow
+
+from base_trader import BaseTrader
+
+class Player(BaseTrader):
+    def show_inventory(self):
+        print(f"\n {self.name}'s Inventory:")
+        print(f"Gold: {self.gold}")
+        for item, qty in self.inventory.items():
+            print(f"{item}: {qty}")
+        print()
+    
